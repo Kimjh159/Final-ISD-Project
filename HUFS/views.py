8944cf09d4bb5ab627c9f40beda859412c3afec8
Kim JungHune <silver9805@hufs.ac.kr>
2023-06-02 13:42:15 +0900

    template test

--- HUFS/views.py
+++ HUFS/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def student(request):
+    return render(request, 'student.html')
+
+
+def timetable(request):
+    return render(request, 'timetable.html')
+
+
+def mocktestanalyze(request):
+    return render(request, 'mocktestanalyze.html')
+
+
+def teacher(request):
+    return render(request, 'teacher.html')
+
+
+def counseling(request):
+    return render(request, 'counseling.html')
+
+
+def visit(request):
+    return render(request, 'visit.html')
+
+
+def leave(request):
+    return render(request, 'leave.html')
--- HUFSACADEMY/urls.py
+++ HUFSACADEMY/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for HUFSACADEMY project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from HUFS import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('student/', views.student, name='student'),
+    path('mocktestanalyze/', views.mocktestanalyze, name='mocktest_analyze'),
+    path('teacher/', views.teacher, name='teacher'),
+    path('timetable/', views.timetable, name='timetable'),
+    path('counseling/', views.counseling, name='counseling'),
+    path('visit/', views.visit, name='visit'),
+    path('leave/', views.leave, name='leave'),
 ]
